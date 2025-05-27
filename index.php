73c58f658a01f0ce4df45804be2bc460c56580c4
FreakyBoo <julianriversandvikendelgado@gmail.com>
2025-05-27 09:11:50 +0000

    Oppdaterte index.php og elbutikk.css: - La til sentrert overskrift og knapper på forsiden i index.php - Opprettet elbutikk.css med sentrert layout, bakgrunnsbilde og stil for knapper

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <title>El butikk - Hjem</title>
+    <link rel="stylesheet" href="elbutikk.css">
+</head>
+<body>
+    <h1>Velkommen til El butikken</h1>
+    <p>Vennligst velg et alternativ for å komme i gang</p>
+
+    <div>
+        <a href="login.php"><button>Logg inn</button></a> 
+        <br>
+        <a href="registrer.php"><button>Registrer deg</button></a>
+    </div>
+</body>
+</html>
